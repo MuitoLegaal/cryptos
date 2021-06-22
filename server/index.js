33e6853f3a91034ffb185504e31e66e6cf30a9e8
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-22 16:06:03 +0200

    dev host

--- server/index.js
+++ server/index.js
@@ -1,28 +1,29 @@
-// const express = require('express');
-// const request = require('request');
-// const API_KEYS = process.env.REACT_APP_API_KEYS;
+const express = require('express');
+const request = require('request');
+const API_KEYS = process.env.REACT_APP_API_KEYS;
 
-// const app = express();
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true })); 
-// const PORT = process.env.PORT || 4000;
+const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+const PORT = process.env.PORT || 4000;
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-// app.post('/data', (req, res) => {
-//   request(
-//     { url: `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=150&convert=${req.body.currencyFromFront}`, headers: { 'X-CMC_PRO_API_KEY': API_KEYS,} },
-//     (error, response, body) => {
-//       if (error || response.statusCode !== 200) {
-//         return res.status(500).json({ type: 'error', message: err.message });
-//       }
-//       res.json(JSON.parse(body));
-//     }
-//   )
-// });
+app.post('/', (req, res) => {
+  request(
+    { url: `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=150&convert=${req.body.currencyFromFront}`, headers: { 'X-CMC_PRO_API_KEY': API_KEYS,} },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      res.json(JSON.parse(body));
+    }
+  )
+});
 
-// app.listen(PORT, () => console.log(`listening on PORT:${PORT}`));
+app.listen(PORT, () => console.log(`listening on PORT:${PORT}`));
--- src/utils/APIs/fetchCoinsData.js
+++ src/utils/APIs/fetchCoinsData.js
@@ -1,9 +1,7 @@
-const API_KEYS = process.env.REACT_APP_API_KEYS;
-
 const fetchCoinsData = async (currency) => {
-  const data = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=150&convert=${currency}`, {
+  const data = await fetch('http://localhost:4000/', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'X-CMC_PRO_API_KEY': API_KEYS },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: `currencyFromFront=${currency}`
   })
   return await data.json()
