5a76571b81633794fa9e099112415b46556a657d
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-22 12:22:43 +0200

    server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const request = require('request');
 const API_KEYS = process.env.REACT_APP_API_KEYS;
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-const path = require('path')
 
 const app = express();
 var bodyParser = require('body-parser');
@@ -17,7 +15,7 @@ app.use((req, res, next) => {
 
 app.post('/data', (req, res) => {
   request(
-    { url: `${BASE_URL}/v1/cryptocurrency/listings/latest?start=1&limit=150&convert=${req.body.currencyFromFront}`, headers: { 'X-CMC_PRO_API_KEY': API_KEYS } },
+    { url: `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=150&convert=${req.body.currencyFromFront}`, headers: { 'X-CMC_PRO_API_KEY': API_KEYS,} },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
@@ -27,12 +25,4 @@ app.post('/data', (req, res) => {
   )
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( '/build' ))
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); 
-  });
-}
-
-app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`listening on PORT:${PORT}`));
