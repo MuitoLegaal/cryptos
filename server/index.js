0850551396954784d86013c1fe8440f75fdaf3ae
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-22 17:12:00 +0200

    env

--- server/index.js
+++ server/index.js
@@ -26,4 +26,22 @@ app.post('/', (req, res) => {
   )
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  app.use(express.static("build"));
+
+  // Express will serve up the front-end index.html file if it doesn't recognize the route
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("build", "index.html"))
+  );
+}
+
+if (process.env.NODE_ENV === "dev") {
+  // Express will serve up production assets
+  app.use(express.static("public"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("public", "index.html"))
+  );
+}
+
 app.listen(PORT, () => console.log(`listening on PORT:${PORT}`));
