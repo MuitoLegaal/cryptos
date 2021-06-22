1efc28a5227d2eacc936531ad9928ad6d7dbdd21
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-22 18:20:46 +0200

    host

--- server/index.js
+++ server/index.js
@@ -28,6 +28,6 @@ app.post('/api', (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST, () => console.log(`listening on PORT:${PORT}`));
