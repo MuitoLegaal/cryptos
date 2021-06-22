bf835edba9bcc25724f692795b85402d86ced1b0
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-22 12:27:39 +0200

    change button theme

--- src/components/backgroundThemeButton.js
+++ src/components/backgroundThemeButton.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
 import { updateThemeAction } from '../actions/actions';
-import { IOSSwitch } from './themeButtonStyle';
 
 function BackgroundThemeButton(props) {
   const [isChecked, setIsChecked] = React.useState(false);
@@ -16,35 +14,11 @@ function BackgroundThemeButton(props) {
   };
 
   return (
-    // <div>
-    //   <Switch
-    //     checked={isChecked}
-    //     onChange={handleChange}
-    //     color="secondary"
-    //     inputProps={{ 'aria-label': 'primary checkbox' }}
-    //   ><span class="toggle_BsTx">🌞</span></Switch>
-    //        </div>
-    <div class="react-toggle displayOnlyInLargeViewport_2aTZ">
-        <Switch
-          checked={isChecked}
-          onChange={handleChange}
-          color="secondary"
-          inputProps={{ 'aria-label': 'primary checkbox' }}
-        />
-        <FormControlLabel
-        control={<IOSSwitch checked={isChecked} onChange={handleChange} />}
-        label="iOS style"
-        />
-      <div class="react-toggle-track">
-              <div class="react-toggle-track-check">
-                <span class="toggle_BsTx">🌜</span>
-              </div>
-              <div class="react-toggle-track-x">
-                <span class="toggle_BsTx">🌞</span>
-              </div>
-      </div>
-    <div class="react-toggle-thumb"></div>
-    </div>
+    <Switch
+      checked={isChecked}
+      onChange={handleChange}
+      color="secondary"
+      inputProps={{ 'aria-label': 'primary checkbox' }} />
 
   );
 }
