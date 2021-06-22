b5f6bd51d2b38a6ae181ef9b9b267ad14ece889c
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-22 15:09:33 +0200

    heroku

--- src/utils/APIs/fetchCoinsData.js
+++ src/utils/APIs/fetchCoinsData.js
@@ -1,7 +1,9 @@
+const API_KEYS = process.env.REACT_APP_API_KEYS;
+
 const fetchCoinsData = async (currency) => {
-  const data = await fetch('http://localhost:4000/data', {
+  const data = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=150&convert=${currency}`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'X-CMC_PRO_API_KEY': API_KEYS },
     body: `currencyFromFront=${currency}`
   })
   return await data.json()
