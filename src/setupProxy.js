875ef5660e187c769436638f22a65838b3620b85
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-23 11:57:38 +0200

    headers proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,14 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'http://localhost:4000',
-//       changeOrigin: true,
-//     })
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:4000',
+      headers: {
+        "Connection": "keep-alive"
+      },
+      changeOrigin: true,
+    })
+  );
+};
