9e29b48e7a956767667a29fb85b28bcc92891440
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-23 11:35:56 +0200

    proxy

--- src/utils/APIs/fetchCoinsData.js
+++ src/utils/APIs/fetchCoinsData.js
@@ -1,7 +1,7 @@
 const fetchCoinsData = async (currency) => {
-  const data = await fetch('http://localhost:4000/api', {
+  const data = await fetch('/api', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded',  "accepts":"application/json" },
     body: `currencyFromFront=${currency}`
   })
   return await data.json()
