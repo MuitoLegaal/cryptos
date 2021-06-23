b793bfbf00f26f5a4f5cb61b4b3b73aaf44d9427
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2021-06-23 14:56:02 +0200

    host replaced

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:4000',
+      target: 'http://[::1]:4000',
       headers: {
         "Connection": "keep-alive"
       },
